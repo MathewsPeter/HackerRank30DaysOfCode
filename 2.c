9ed2064ab0306641ba7cc860ecc2d8b5946e31f4
Mathews Peter <mathewspeter27@gmail.com>
2021-05-02 14:03:30 +0530

    2

---
+++ 2.c
@@ -0,0 +1,27 @@
+//Day 2: Operators
+
+
+int main()
+{
+    char* meal_cost_endptr;
+    char* meal_cost_str = readline();
+    double meal_cost = strtod(meal_cost_str, &meal_cost_endptr);
+
+    if (meal_cost_endptr == meal_cost_str || *meal_cost_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* tip_percent_endptr;
+    char* tip_percent_str = readline();
+    int tip_percent = strtol(tip_percent_str, &tip_percent_endptr, 10);
+
+    if (tip_percent_endptr == tip_percent_str || *tip_percent_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* tax_percent_endptr;
+    char* tax_percent_str = readline();
+    int tax_percent = strtol(tax_percent_str, &tax_percent_endptr, 10);
+
+    if (tax_percent_endptr == tax_percent_str || *tax_percent_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
